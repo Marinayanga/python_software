c52a3c4d6698d92d619e85b93e52c18bca3caf2c
Марина <marinayanga@gmail.com>
2019-07-11 16:19:44 +0300

    18. Реализовать генераторы тестовых данных для групп и контактов, сохраняющие созданные данные в файл в формате JSON.

--- conftest.py
+++ conftest.py
@@ -2,7 +2,8 @@ import pytest
 from fixture.application import Application
 import json
 import os.path
-import imaplib
+import importlib
+import jsonpickle
 fixture = None
 target = None
 
@@ -36,13 +37,20 @@ def pytest_addoption(parser):
     parser.addoption("--target", action="store", default="target.json")
 
 
-def pytest_generate_test(metafunc):
+def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
-        if fixture.startwith("data_"):
+        if fixture.startswith("data_"):
             testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
 
 
 def load_from_module(module):
-    return imaplib.import_module("data.%s:" % module).testdata
+    return importlib.import_module("data.%s" % module).testdata
 
+
+def load_from_json(file):
+     with open (os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
---
+++ generator/contact.py
@@ -0,0 +1,48 @@
+import getopt
+import sys
+from model.contact import Contact
+import string
+import random
+import os.path
+import jsonpickle
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+        # print help information and exit:
+    print(err)  # will print something like "option -a not recognized"
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/contact.json"
+
+for o, a in opts:
+    if o == "-n":
+        n=int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_string(prefix,maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", middlename="", lastname="", nickname="", title="", mobile="",
+                   email="", company="", address="", home="", work="",fax="", email2="", email3="", homepage="",
+                   bday="", bmonth="-", byear="", aday="", amonth="-", ayear="",address2="", phone2="", notes="")] + \
+          [Contact(firstname=random_string("name", 5), middlename=random_string("middlename",5), lastname=random_string("last",10),
+                   nickname=random_string("nick", 7), title=random_string("title", 5), mobile=random_string("8900",7),
+                   email=random_string("a@a.", 3), company=random_string("company", 7),
+                   address=random_string("Moscow", 7), home=random_string("8900",7), work=random_string("323537", 5),
+                   fax=random_string("12345",5), email2=random_string("a@a.", 3),
+                   email3=random_string("a@a.", 3), homepage=random_string("fdfdfdfdf", 3), bday="15", bmonth="July",
+                   byear=random_string("2",3),aday="17", amonth="April", ayear=random_string("2",3), address2=random_string("Moscow", 7),
+                   phone2=random_string("8900",7),notes=random_string("notes", 7)) for i in range(5)]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..",f)
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent = 2)
+    out.write(jsonpickle.encode(testdata))
